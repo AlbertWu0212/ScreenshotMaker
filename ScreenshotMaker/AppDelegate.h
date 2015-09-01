fdb0aa330024f76719f7b2557e832236945e7629
wuzhengbin <wzbdroid@gmail.com>
2015-09-01 20:17:01 +0800

    Initial Commit

---
+++ ScreenshotMaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenshotMaker
+//
+//  Created by WuZhengBin on 15/9/1.
+//  Copyright (c) 2015年 WuZhengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
