a78c16f1f47f291369e360085b6edde1a8eefa63
wuzhengbin <wzbdroid@gmail.com>
2015-09-01 20:19:56 +0800

    Add ViewController

---
+++ ScreenshotMaker/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ScreenshotMaker
+//
+//  Created by WuZhengBin on 15/9/1.
+//  Copyright (c) 2015年 WuZhengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
